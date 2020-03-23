d7131758ca2f4e0d3963c80c1ed88ea1b8224181
totoro7 <abcde472@naver.com>
2020-03-23 17:44:14 +0900

    push

---
+++ src/Component1.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import axios from 'axios';
+import {useReducer, useEffect} from 'react';
+import './Component.css';
+
+export function reducer(state,action){
+  switch(action.type){
+    case 'LOADING':
+      return { loading : true, data:null, error:null};
+    case 'SUCCESS':
+      console.log(action.data);
+      return {loading : false, data: action.data, error: null };
+    case 'ERROR':
+      return {loading : false, data:null, error:null};
+    default:
+      throw new Error('unhandled action type ${action.type}');
+
+  }
+}
+
+
+function Component1() {
+  var url="abc";
+  const array =[];
+  var value=encodeURI("공부asmr");
+  const youtubeurl='https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDFsv1j21WwWmaZyQawt64f_RosQTC6WTY&maxResults=5&q="'+value+'"';
+
+  const[state,dispatch]=useReducer(reducer,{loading: false, data: null, error: null});
+  
+  const fetchusers = async()=>{
+    dispatch({type:'LOADING'});
+    try{
+      const response = await axios.get(youtubeurl);
+      dispatch({type:'SUCCESS', data:response.data})
+      console.log(response.data);
+    }
+    catch(e){
+      dispatch({type:'ERROR',error:e});
+    }
+  }
+
+  useEffect(()=>{
+    fetchusers();
+  },[]);
+
+  const { loading, data:users, error } = state;
+  if(loading) return <div>로딩중..</div>;
+  if(error) return <div>에러 발생</div>;
+  if(users){
+    console.log(users);
+    for(let i=0;i<5;i++){
+      var abc = users.items[i].id.videoId;
+      url="https://www.youtube.com/embed/"+abc;
+      array.push(url);
+    }
+    
+  }
+  
+  
+
+
+  
+  return (
+    
+    <div className="d">
+      <div className="title">공부 asmr</div>
+      <div>
+      {array.map(urls => (
+        <iframe name="y" id="ytplayer" type="text/html" width="320" height="180" frameborder="0"
+        src={urls}/>
+      ))}
+      </div>
+    </div>
+  );
+
+}
+
+
+
+export default Component1;
